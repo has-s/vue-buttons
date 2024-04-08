53b778fe68c9c4e70d086b3b1a661ac5a311de1d
has-s <auttenberg@icloud.com>
2024-04-08 21:29:19 +0300

    , _,

---
+++ vue/vue/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from '@/components/App.vue'
+import router from './router';
+import Vuex from 'vuex'
+import storeRoot from './store'
+import { sync } from "vuex-router-sync";
+
+import '@fortawesome/fontawesome-free/css/all.css'
+
+Vue.use(Vuex)
+Vue.config.productionTip = false
+
+
+const store = new Vuex.Store(storeRoot)
+sync(store, router);
+
+new Vue({
+  render: h => h(App),
+  router,
+  store
+}).$mount('#app')
